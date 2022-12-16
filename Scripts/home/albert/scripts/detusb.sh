7a09444e036007f627ffbe103759f11fd55aaab1
Arkhelan <albertperezrovira26@gmail.com>
2022-12-16 16:16:18 +0100

    cambios

--- Scripts/home/albert/scripts/detusb.sh
+++ Scripts/home/albert/scripts/detusb.sh
@@ -1,32 +1,30 @@
 # !/bin/bash
-for (( ; ; ))
+echo "el script se ha iniciado" >> /home/albert/fin/registro
+for (( ; ; ));
 do
-a=0
+    echo "el script ha dado una vuelta" >> /home/albert/fin/registro
+    a=0
+    sleep 1
     for i in $(ls /dev/sd*)
 	    do
-            if [[ $i != "/dev/sda" ]] && [[ $i != "/dev/sda1" ]] && [[ $i != "/dev/sda2" ]] && [[ $i != "/dev/sda3" ]]
+            if [ $i != "/dev/sda" ] && [ $i != "/dev/sda1" ] && [ $i != "/dev/sda2" ] && [ $i != "/dev/sda3" ];
                 then
                     a=$(echo $i)
                 fi
 	    done
-    if [[ $a != "0" ]]
+    if [ $a != "0" ];
+    then
+        b=${a:-1}
+        if [ "$b" == "1" ];
         then
-            echo $a
-            break
+                a=$a"1"
         fi
+        echo $a
+        break
+    fi
 done
-b=$(lsblk -o FSTYPE $a)
-b=${b/FSTYPE}
-
-case $(echo $b) in
-    vfat)
-        sudo mount -t vfat $a /media/usb
-        ;;
-    ntfs)
-        sudo mount -t ntfs-3g $a /media/usb
-        ;;
-    ext4)
-        sudo mount -t ext4 $a /media/usb
-        ;;
-esac 
+mount $a /media/usb
 sudo chmod 777 /media/usb
+#sh /home/albert/fin/scriptfinal.sh
+echo "el script ha finalizado" >> /home/albert/fin/registro
+mount $a /media/usb
